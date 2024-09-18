872996009f2598dd32bbc646d5e43a8227063fb4
redwan-ai <ahmedredwan68@gmail.com>
2024-09-19 01:23:38 +0600

    second commit

--- js/login.js
+++ js/login.js
@@ -2,4 +2,7 @@ document.getElementById('btn-login')
 .addEventListener('click', function(event){
     event.preventDefault(); //--barbar reload na neyar jonno lekhsi
     console.log('login button clicked');
+
+    const phoneNumber = document.getElementById('phone-number').value;
+    console.log('Phone number show');
 })
